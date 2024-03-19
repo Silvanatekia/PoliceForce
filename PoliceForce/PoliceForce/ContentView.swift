ca7cac6219dfbb94469db89627f5f9068a683fa9
Silvanatekia <silvanatekia0@gmail.com>
2024-03-19 12:26:56 +0000

    updated

--- PoliceForce/PoliceForce/ContentView.swift
+++ PoliceForce/PoliceForce/ContentView.swift
@@ -1,58 +1,62 @@
-//
-//  ContentView.swift
-//  PoliceForce
-//
-//  Created by Silvana Tekia on 12/03/2024.
-//
 
 import SwiftUI
 
-        struct PoliceForce: Codable {
-            let id: String
-            let name: String
-            
-        }
+struct PoliceForce: Codable {
+    let id: String
+    let name: String
+}
 
-        struct ContentView: View {
-            @State private var forces = [PoliceForce]()
+struct ContentView: View {
+    @State private var forces = [PoliceForce]()
 
-            var body: some View {
-                NavigationView {
-                    List(forces, id: \.id) { force in
-                        NavigationLink(destination: Text("Details for \(force.name)")) {
-                            Text(force.name)
-                        }
-                    }
-                    .navigationTitle("Police Forces")
-                }
-                .onAppear {
-                    fetchData()
+    var body: some View {
+        NavigationView {
+            List(forces, id: \.id) { force in
+                NavigationLink(destination: Text("Details for \(force.name)")) {
+                    Text(force.name)
                 }
             }
-
-            func fetchData() {
-                guard let url = URL(string: "https://data.police.uk/api/forces") else {
-                    print("Invalid URL")
-                    return
+            .navigationTitle("Police Forces")
+            .task {
+                do {
+                    forces = try await fetchData()
+                } catch {
+                    print("Error: \(error)")
                 }
-
-                URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data {
-                        do {
-                            let decodedData = try JSONDecoder().decode([PoliceForce].self, from: data)
-                            DispatchQueue.main.async {
-                                self.forces = decodedData
-                            }
-                        } catch {
-                            print("Error decoding JSON: \(error)")
-                        }
-                    } else {
-                        print("Invalid Data")
-                    }
-                }.resume()
             }
         }
+    }
 
-#Preview {
-    ContentView()
+    func fetchData() async throws -> [PoliceForce] {
+        let endpoint = "https://data.police.uk/api/forces"
+        guard let url = URL(string: endpoint) else {
+            throw PFError.InvalidURL
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw PFError.InvalidResponse
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([PoliceForce].self, from: data)
+        } catch {
+            throw PFError.InvalidData
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+enum PFError: Error {
+    case InvalidURL
+    case InvalidResponse
+    case InvalidData
 }
